cd73772db185dcf9cca44b0f965600331472b792
Mradul Umrao <mradulumrao@gmail.com>
2026-06-14 02:26:05 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,31 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY", // Prevents your site from being framed (Clickjacking protection)
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff", // Prevents MIME-type sniffing
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          }
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
